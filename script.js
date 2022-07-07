aff04f0ac52ec9e66e6e8a6f64aca1abd23ce858
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-07-06 20:33:38 -0600

    Sliding functionality added w/ JS for product selection & some styling

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// js file
+const sliderWrapper = document.querySelector(".sliderWrapper");
+const listItems = document.querySelectorAll(".listItem");
+
+console.log(sliderWrapper);
+//sliderWrapper.style.transform = "translateX(00vw)"; //initial point starts from px value
+
+listItems.forEach((item, index) => {
+     item.addEventListener("click", ()=> {
+        sliderWrapper.style.transform = `translateX(${-100 * index}vw)`;
+     });
+});
